168d975e315382754635a61e8b32f1ffc098e285
Kauan Decarli <kauandecarli@gmail.com>
2025-04-05 23:19:50 -0300

    feat: handle SIGINT/SIGTERM on api

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,14 @@
 import { createServer } from "./app"
 import { env } from "./config/env.config"
 
+function handleExit(signal: string) {
+  console.log(`Received ${signal}`)
+  process.exit()
+}
+
+process.on("SIGINT", handleExit)
+process.on("SIGTERM", handleExit)
+
 const start = async () => {
   try {
     const app = await createServer()
