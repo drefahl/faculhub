33b85040540e7ac90e003f05d9e1842c7724d85e
Cauã Drefahl <cauadrefahl@gmail.com>
2025-04-07 20:42:27 -0300

    feat: update CORS configuration for production environment

--- api/src/config/cors.config.ts
+++ api/src/config/cors.config.ts
@@ -1,10 +1,12 @@
 import fastifyCors from "@fastify/cors"
 import type { FastifyInstance } from "fastify"
+import { env } from "./env.config"
 
 export function corsConfig(app: FastifyInstance) {
   app.register(fastifyCors, {
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 }
