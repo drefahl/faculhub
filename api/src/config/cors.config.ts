ef4f3ef1720d00a08031465d16e72af9a7b75e83
Kauan Decarli <kauandecarli@gmail.com>
2025-04-09 18:38:37 -0300

    fix: allow credentials on CORS

--- api/src/config/cors.config.ts
+++ api/src/config/cors.config.ts
@@ -6,5 +6,6 @@ export function corsConfig(app: FastifyInstance) {
     origin: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 }
