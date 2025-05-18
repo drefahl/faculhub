7f245cbda3894a1dd293ec062fe97621867a983d
Kauan Decarli <kauandecarli@gmail.com>
2025-05-18 13:33:59 -0300

    fix: add alias for email on vitest config

--- api/vitest.config.ts
+++ api/vitest.config.ts
@@ -1,8 +1,12 @@
+import { resolve } from "node:path"
 import tsconfigPaths from "vite-tsconfig-paths"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  resolve: {
+    alias: [{ find: "email", replacement: resolve(__dirname, "./email/dist") }],
+  },
   test: {
     globals: true,
     environment: "node",
